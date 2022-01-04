8a58336f383cb648deff353438526d679bc86d27
designerAmit123 <amitkadu7@gmail.com>
2022-01-04 18:01:42 +0530

    Addes top-navigation bar and header
    Addes top-navigation bar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { TopNavigationComponent } from './template/top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule,
